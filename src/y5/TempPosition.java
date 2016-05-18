ac6afa5cef34e2aab38cb52144bdd4b55c74b8b4
coilx <coilx@10.20.171.183>
2016-05-18 10:13:01 +0200

    Started coding the Boatplacement method

---
+++ src/y5/TempPosition.java
@@ -0,0 +1,35 @@
+package y5;
+
+/**
+ *
+ * @author Tobias Edit Dec 2015 Peter Lorensen: -added JavaDoc comments
+ */
+public class TempPosition {
+
+    private int x;
+    private int y;
+    
+    
+
+    public TempPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
